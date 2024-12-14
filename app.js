244bbaf2fce806ee4acedd2615d3fc5d9090b459
Kayla Harris <103599117+k281harri@users.noreply.github.com>
2024-12-14 00:46:09 -0600

    4

--- app.js
+++ app.js
@@ -7,3 +7,23 @@
 thisForm.addEventListener('submit', Name)
 
 const  
+
+// append 
+listo.appendChild(thisForm)
+
+}
+function validateEmail() {
+    if (email.value === "") {
+      window.alert("Please provide an email address.");
+      email.focus();
+      return false;
+    }
+    return email.value;
+
+}
+
+const browser = document.createElement("p");
+  browser.innerHTML = ` 
+    ${window.navigator.appCodeName} | 
+    ${window.navigator.appName} | 
+    ${version}`;
